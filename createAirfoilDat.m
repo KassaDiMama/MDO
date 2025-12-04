7a71eb652dda68ae3588a4e7cd1e7948455b0b71
Kasper Weel <kasper.weel@to70.eu>
2025-12-04 12:41:33 +0100

    added cst dat file generation

--- createAirfoilDat.m
+++ createAirfoilDat.m
@@ -1,17 +1,31 @@
-function [t_lower, y_lower, t_upper,y_upper] = createAirfoilDat(N1, N2, AL,AU)
+function [t_upper,y_upper,t_lower, y_lower] = createAirfoilDat(N1, N2, AU,AL,fileName)
     function result = CST(t,A)
         cn = t^N1 * (1-t)^N2;
         s = 0;
         for i = 0:5
-            si = nchoosek(5,0)*t^i*(1-t)^(5-i)*A(i);
+            si = nchoosek(5,0)*t^i*(1-t)^(5-i)*A(i+1);
             s=s+si;
         end
         result = cn*s;
     end
-    points_per_side = 23;
+    points_per_side = 46;
+
+    t_upper = [];
+    y_upper = [];
+    for index = 0:points_per_side
+        t = index/points_per_side;
+        y = CST(t, AU);
+
+        t_upper = [t_upper, t];
+        y_upper = [y_upper, y];
+    end
+    
+    t_upper = flip(t_upper);
+    y_upper = flip(y_upper);
+
     t_lower = [];
     y_lower = [];
-    for index = 0:points_per_side-1
+    for index = 0:points_per_side
         t = index/points_per_side;
         y = CST(t,AL);
 
@@ -19,13 +33,24 @@ function [t_lower, y_lower, t_upper,y_upper] = createAirfoilDat(N1, N2, AL,AU)
         y_lower = [y_lower, y];
     end
     % Calculate upper surface points using a similar approach
-    for index = 1:points_per_side-2
-        t = index/points_per_side;
-        y = CST(t, AU);
+    
+    
+    fid = fopen(fileName+'.dat', 'w');
 
-        t_upper = [t_upper, t];
-        y_upper = [y_upper, y];
+    if fid == -1
+        error('Cannot open boeing.init for writing.');
     end
+    for index = 1:length(t_upper)
+        t = t_upper(index);
+        y = y_upper(index);
+        fprintf(fid, string(t)+" "+string(y)+"\n");
+    end
+    for index = 1:length(t_lower)
+        t = t_lower(index);
+        y = y_lower(index);
+        fprintf(fid, string(t)+" "+string(y)+"\n");
+    end
+    fclose(fid);
 end
 
 
